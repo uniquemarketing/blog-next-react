6d34fbb3b2021607fc6ce9ff1d1d6238c5204296
Anderson Andrade <anderson@uniquemarketing.com.br>
2025-07-08 18:58:21 -0300

    Component Heading dinâmico

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,43 @@ import { Container } from "@/components/Container";
 import { Header } from "@/components/Header";
 import { PostsList } from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
+import Link from "next/link";
+import Image from "next/image";
 import { Suspense } from "react";
+import { PostHeading } from "@/components/PostHeading";
 
 export default async function HomePage() {
   return (
     <Container>
       <Header />
+
+      <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
+        <Link className="w-full h-full overflow-hidden rounded-xl" href="#">
+          <Image
+            className="w-full h-full object-cover object-center group-hover:scale-105 transition"
+            src="/images/bryen_0.png"
+            alt="Título do Post"
+            width={1200}
+            height={720}
+            priority
+          />
+        </Link>
+        <div className="flex flex-col gap-3 sm:justify-center">
+          <time
+            className="block text-slate-600 text-sm/tight"
+            dateTime="2025-04-20"
+          >
+            20/04/2025 10:00
+          </time>
+          <PostHeading url="#" as="h2">
+            exercitationem tempora sint ipsum?
+          </PostHeading>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+          molestias sequi recusandae ratione cum dolore illo explicabo corrupti
+          esse suscipit deleniti, maxime omnis officia facilis! Provident
+          exercitationem tempora sint ipsum?
+        </div>
+      </section>
 
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
---
+++ src/components/PostHeading/index.tsx
@@ -0,0 +1,23 @@
+import clsx from "clsx";
+import Link from "next/link";
+type PostHeadingPropos = {
+  children: React.ReactNode;
+  url: string;
+  as?: "h1" | "h2";
+};
+export function PostHeading({
+  children,
+  url,
+  as: Tag = "h2",
+}: PostHeadingPropos) {
+  const headingClassMap = {
+    h1: "text-2xl/tight  mb-4 sm:text-3xl",
+    h2: "text-2xl/tight  mb-4 sm:text-3xl",
+  };
+  const commonClass = "font-extrabold";
+  return (
+    <Tag className={clsx(headingClassMap[Tag], commonClass)}>
+      <Link href={url}>{children}</Link>
+    </Tag>
+  );
+}
