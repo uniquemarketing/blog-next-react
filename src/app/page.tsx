afac2c6ac597c146850b70621edfd0b58a980e3a
Anderson Andrade <anderson@uniquemarketing.com.br>
2025-07-08 20:36:13 -0300

    Grid responsiva de PostList com tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default async function HomePage() {
             width: 1200,
             height: 720,
             src: "/images/bryen_9.png",
-            alt: "Alta da imagem",
+            alt: "",
             priority: true,
           }}
         />
