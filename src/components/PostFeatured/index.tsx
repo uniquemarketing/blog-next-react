4f504cffef4de3a9926aca5175a516ad60f965e0
Anderson Andrade <anderson@uniquemarketing.com.br>
2025-07-09 14:58:21 -0300

    Dicas de segurança sobre props no Next.js

--- src/components/PostFeatured/index.tsx
+++ src/components/PostFeatured/index.tsx
@@ -1,36 +1,19 @@
-import { PostCoverImage } from "../PostCoverImage";
-import { PostHeading } from "../PostHeading";
+import { PostSummary } from "../PostSummary";
 
 export function PostFeatured() {
   const slug = "";
   const postLink = `/post/${slug}`;
   return (
     <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
-      <PostCoverImage
-        linkProps={{ href: postLink }}
-        imageProps={{
-          width: 1200,
-          height: 720,
-          src: "/images/bryen_9.png",
-          alt: "",
-          priority: true,
-        }}
+      <PostSummary
+        postLink={postLink}
+        postHeading="h1"
+        createdAt={"2025-04-08T00:24:38.616Z"}
+        excerpt={
+          "O Next.js também é uma boa escolha para quem quer se preocupar com performance e SEO."
+        }
+        title={"Rotina matinal de pessoas altamente eficazes"}
       />
-      <div className="flex flex-col gap-3 sm:justify-center">
-        <time
-          className="block text-slate-600 text-sm/tight"
-          dateTime="2025-04-20"
-        >
-          20/04/2025 10:00
-        </time>
-        <PostHeading url={postLink} as="h2">
-          exercitationem tempora sint ipsum?
-        </PostHeading>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
-        molestias sequi recusandae ratione cum dolore illo explicabo corrupti
-        esse suscipit deleniti, maxime omnis officia facilis! Provident
-        exercitationem tempora sint ipsum?
-      </div>
     </section>
   );
 }
--- src/components/PostsList/index.tsx
+++ src/components/PostsList/index.tsx
@@ -1,6 +1,6 @@
 import { postRepository } from "@/repositories/post";
 import { PostCoverImage } from "../PostCoverImage";
-import { PostHeading } from "../PostHeading";
+import { PostSummary } from "../PostSummary";
 
 export async function PostsList() {
   const posts = await postRepository.findAll();
@@ -22,20 +22,13 @@ export async function PostsList() {
               }}
             />
 
-            <div className="flex flex-col gap-4 sm:justify-center">
-              <time
-                className="text-slate-600 block text-sm/tight"
-                dateTime={post.createdAt}
-              >
-                {post.createdAt}
-              </time>
-
-              <PostHeading as="h2" url={postLink}>
-                {post.title}
-              </PostHeading>
-
-              <p>{post.excerpt}</p>
-            </div>
+            <PostSummary
+              postLink={postLink}
+              postHeading="h2"
+              createdAt={post.createdAt}
+              excerpt={post.excerpt}
+              title={post.title}
+            />
           </div>
         );
       })}
